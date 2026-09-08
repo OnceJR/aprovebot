9c50d1393ba472478199e4996b7759d32bbc1b8a
OnceJR <131706454+OnceJR@users.noreply.github.com>
2026-09-08 18:08:57 -0300

    Update iobot.py

--- iobot.py
+++ iobot.py
@@ -1560,7 +1560,7 @@ async def main():
     global db_client, db, backup_queue
     # 1. Inicializar bases de datos y colas asincrónicas DENTRO del Event Loop
     db_client = AsyncIOMotorClient(MONGO_URI)
-    db = db_client.intercambio_bot_v6
+    db = db_client.intercambio_bot_v7
     backup_queue = asyncio.Queue()
     
     # 2. Configurar el bot y el servidor
